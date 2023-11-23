47203a85c15503b6234e97d437f8c8051ed2f533
Trawirr <gtrawirr@gmail.com>
2023-11-24 00:08:01 +0100

    Models updated; abstract Ingredient model created

--- Calculator/admin.py
+++ Calculator/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-@admin.register(Ingredient)
-class IngredientAdmin(admin.ModelAdmin):
-    list_display = ("name", "protein", "carbs", "fat", "kcal")
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ("name", "protein", "carbs", "fat", "kcal", "piece_factor")
+    ordering = ("name", "kcal")
+    
+@admin.register(Drink)
+class Drink(admin.ModelAdmin):
+    list_display = ("name", "protein", "carbs", "fat", "kcal", "cup_factor")
     ordering = ("name", "kcal")
--- Calculator/models.py
+++ Calculator/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Ingredient(models.Model):
+    class Meta:
+        abstract = True
+
     name = models.CharField(max_length=50, default="New ingredient")
-    protein = models.FloatField(default = 0, verbose_name="protein/100g")
-    carbs = models.FloatField(default = 0, verbose_name="carbs/100g")
-    fat = models.FloatField(default = 0, verbose_name="fat/100g")
-    kcal = models.FloatField(default = 0, verbose_name="kcal/100g")
+    protein = models.FloatField(default=0, verbose_name="protein/100g_ml")
+    carbs = models.FloatField(default=0, verbose_name="carbs/100g_ml")
+    fat = models.FloatField(default=0, verbose_name="fat/100g_ml")
+    kcal = models.FloatField(default=0, verbose_name="kcal/100g_ml")
+
+class Food(Ingredient):
+    piece_factor = models.IntegerField(default=0, verbose_name="Avg piece weight")
+
+class Drink(Ingredient):
+    pass
+
+    # --ADD-- methods for spoons, cups etc.
